eb05e7fa6ac0a10a3bcfe4b67392b37fde3b53d2
youngzhenhao <2499342961@qq.com>
2024-03-18 20:22:32 +0800

    update

--- code/back-end/wallet/lnurl/lnurl.go
+++ code/back-end/wallet/lnurl/lnurl.go
@@ -9,23 +9,19 @@ import (
 
 func LnurlRouterRun() {
 	router := setupRouter()
-	router.Run(":8888")
+	router.Run(":8080")
 }
 
-// setupRouter
-//
-//	@Description: 请求服务器的LND节点开发票
-//	@return *gin.Engine
 func setupRouter() *gin.Engine {
 	router := gin.Default()
-	router.LoadHTMLGlob("templates/*")
+	router.LoadHTMLGlob("lnurl/page/pay.html")
 	//router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
 	router.GET("/pay", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "pay.html", gin.H{
 			"title": "pay",
 		})
 	})
-
+	//  请求服务器的LND节点开发票
 	router.POST("/addInvoice", func(c *gin.Context) {
 		amountStr := c.PostForm("amount")
 		amountInt, _ := strconv.Atoi(amountStr)

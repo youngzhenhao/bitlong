037e53b433b7010086be0ded3c05b69c88566ce3
youngzhenhao <2499342961@qq.com>
2024-03-25 11:03:34 +0800

    mod TmpTestApi.go

--- code/back-end/wallet/api/TmpTestApi.go
+++ code/back-end/wallet/api/TmpTestApi.go
@@ -6,27 +6,24 @@ import (
 	"os"
 )
 
-// CreateFileWithContent 在指定的目录中创建一个新的txt文件，并写入内容
-func CreateFileWithContent(dir, filename, content string) error {
+func CreateFileWithContent(dir, filename, content string) bool {
 	if _, err := os.Stat(dir); os.IsNotExist(err) {
 		err := os.MkdirAll(dir, 0777)
 		if err != nil {
-			return fmt.Errorf("MkdirAll error: %v", err)
+			fmt.Printf("MkdirAll error: %v", err)
+			return false
 		}
 	}
 	filePath := dir + "/" + filename + ".txt"
 	err := os.WriteFile(filePath, []byte(content), 0777)
 	if err != nil {
-		return fmt.Errorf("WriteFile error: %v", err)
+		fmt.Printf("WriteFile error: %v", err)
+		return false
 	}
 	fmt.Printf("Successed!\n")
-	return nil
+	return true
 }
 func TESTWriteFile() bool {
 	folderPath := base.Configure("tapd")
-	err := CreateFileWithContent(folderPath, "TESTWriteFile", "TEST MESSAGE.")
-	if err != nil {
-		return false
-	}
-	return true
+	return CreateFileWithContent(folderPath, "TESTWriteFile", "TEST MESSAGE.")
 }

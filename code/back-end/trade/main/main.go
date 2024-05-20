2c8f21c039e44bf0e26541ff8eb3f848d52ffdf7
youngzhenhao <2499342961@qq.com>
2024-05-20 17:35:23 +0800

    update main

--- code/back-end/trade/main/main.go
+++ code/back-end/trade/main/main.go
@@ -3,58 +3,40 @@ package main
 import (
 	"fmt"
 	"github.com/robfig/cron/v3"
-	"log"
 	"trade/config"
 	"trade/dao"
 	"trade/routers"
 	"trade/task"
+	"trade/utils"
 )
 
 func main() {
-
 	loadConfig, err := config.LoadConfig("config.yaml")
 	if err != nil {
-		panic("failed to load config: " + err.Error())
+		utils.LogError("failed to load config: ", err)
 	}
-
-	if true {
-		if loadConfig.Routers.Login {
-			// Initialize the database connection
-
-		}
+	if loadConfig.Routers.Login {
 		dao.InitMysql()
 		dao.RedisConnect()
-
-		jobs, err := task.LoadJobs()
+	}
+	err = dao.Migrate()
+	jobs, err := task.LoadJobs()
+	if err != nil {
+		utils.LogError("", err)
+	}
+	c := cron.New(cron.WithSeconds())
+	for _, job := range jobs {
+		_, err := c.AddFunc(job.CronExpression, func() {
+			task.ExecuteWithLock(job.Name)
+		})
 		if err != nil {
-			log.Fatal(err)
+			utils.LogError("Error scheduling job "+job.Name, err)
 		}
-
-		c := cron.New(cron.WithSeconds())
-		for _, job := range jobs {
-			// Schedule each job using cron
-			_, err := c.AddFunc(job.CronExpression, func() {
-				task.ExecuteWithLock(job.Name)
-			})
-			if err != nil {
-				log.Printf("Error scheduling job %s: %v\n", job.Name, err)
-			}
-		}
-
-		c.Start()
-		r := routers.SetupRouter()
-		// Read the port number from the configuration file
-
-		port := loadConfig.GinConfig.Port
-		if port == "" {
-			// Default port number
-			port = "8080"
-		}
-		// Start the server
-		err = r.Run(fmt.Sprintf(":%s", port))
-		if err != nil {
-			return
-		}
+	}
+	c.Start()
+	if err != nil {
+		utils.LogError("AutoMigrate error", err)
+		return
 	}
 	r := routers.SetupRouter()
 	bind := loadConfig.GinConfig.Bind
@@ -63,4 +45,7 @@ func main() {
 		port = "8080"
 	}
 	err = r.Run(fmt.Sprintf("%s:%s", bind, port))
+	if err != nil {
+		return
+	}
 }

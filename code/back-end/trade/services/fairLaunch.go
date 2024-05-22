b0a2430126b22d1b097ae14a7f33d3d382b3262a
youngzhenhao <2499342961@qq.com>
2024-05-22 12:37:26 +0800

    update fair launch

--- code/back-end/trade/services/fairLaunch.go
+++ code/back-end/trade/services/fairLaunch.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"net/http"
 	"net/url"
+	"strconv"
 	"trade/api"
 	"trade/config"
 	"trade/middleware"
@@ -90,22 +91,20 @@ func GetAddr(id string, amt int) string {
 	return ""
 }
 
-// TODO: Need to test
-func PostPhoneToNewAddr(remotePort string, assetId string, amount string) *taprpc.Addr {
+func PostPhoneToNewAddr(remotePort string, assetId string, amount int) *taprpc.Addr {
 	frpsForwardSocket := fmt.Sprintf("%s:%s", config.GetLoadConfig().FrpsServer, remotePort)
-	targetUrl := "http://" + frpsForwardSocket + "/addInvoice"
-	payload := url.Values{"asset_id": {assetId}, "amount": {amount}}
+	targetUrl := "http://" + frpsForwardSocket + "/newAddr"
+	payload := url.Values{"asset_id": {assetId}, "amount": {strconv.Itoa(amount)}}
 	response, err := http.PostForm(targetUrl, payload)
 	if err != nil {
 		utils.LogError("http.PostForm.", err)
 		return nil
 	}
 	bodyBytes, _ := io.ReadAll(response.Body)
-
 	var addrResponse struct {
-		Success bool         `json:"success"`
-		Error   string       `json:"error"`
-		Data    *taprpc.Addr `json:"data"`
+		Success bool   `json:"success"`
+		Error   string `json:"error"`
+		Data    *taprpc.Addr
 	}
 	if err := json.Unmarshal(bodyBytes, &addrResponse); err != nil {
 		utils.LogError("PPTNA json.Unmarshal.", err)

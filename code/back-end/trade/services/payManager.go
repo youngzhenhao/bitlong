3e5f807baf2292853e5f70cabba9a9ca9dd58bb2
SevenLiquor <1013871560@qq.com>
2024-05-24 17:35:51 +0800

    Trade: feat:
    fix:
    1.
    Wallet:
    feat:
    1.增加
    fix:
    1.

--- code/back-end/trade/services/payManager.go
+++ code/back-end/trade/services/payManager.go
@@ -7,8 +7,8 @@ func NewRecharge() {
 }
 
 // 托管账户划扣费用
-func PayAmountToAdmin(payUserId uint, amount uint64) (uint, error) {
-	id, err := PayAmountInside(payUserId, adminUserId, amount)
+func PayAmountToAdmin(payUserId uint, gasFee, serveFee uint64) (uint, error) {
+	id, err := PayAmountInside(payUserId, adminUserId, gasFee, serveFee)
 	if err != nil {
 		CUST.Error("PayAmountToAdmin failed:%s", err)
 		return 0, err

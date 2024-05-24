d7748cba6bbac263f408f316bf18333b9905e637
SevenLiquor <1013871560@qq.com>
2024-05-24 14:58:34 +0800

    feat:
    fix:
    1.修正上一次提交的一些错误

--- code/back-end/trade/services/cronService.go
+++ code/back-end/trade/services/cronService.go
@@ -1,3 +1,12 @@
 package services
 
 type CronService struct{}
+
+func (sm *CronService) PollPaymentCron() {
+	CUST.Info("start cron job: PollPayment")
+	pollPayment()
+}
+func (sm *CronService) PollInvoiceCron() {
+	CUST.Info("start cron job: PollInvoice")
+	pollInvoice()
+}

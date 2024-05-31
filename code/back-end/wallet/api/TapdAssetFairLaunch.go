05300c1895cf850cf4a14305e74527a8b3778e0a
youngzhenhao <2499342961@qq.com>
2024-05-31 17:43:03 +0800

    fix: refactor file name and add todo

---
+++ code/back-end/wallet/api/TapdAssetFairLaunch.go
@@ -0,0 +1,5 @@
+package api
+
+// TODO: Assemble local and server asset issuance data
+// Use new makeJsonResult
+// TODO: Calculate the handling fee and determine whether the handling fee is sufficient
